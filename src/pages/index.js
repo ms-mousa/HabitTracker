a19d67fe997456076e136bb2dc82732ef9b790ed
ms-mousa <m.shehata91@gmail.com>
2019-12-16 16:45:39 +1100

    Initial Setup

---
+++ src/pages/index.js
@@ -0,0 +1,19 @@
+import styled from '@emotion/styled'
+
+
+const Index = (props) => {
+    const Container = styled.div`
+        background:black;
+        color:blue;
+    `
+    return (
+        <Container>
+            <h1>
+                Hello, stack
+            </h1>
+        </Container>
+
+    );
+}
+
+export default Index;
